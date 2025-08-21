0d7990cfa82cd48ef13e61ac034030deb207c006
dineshkumar-k <dhineshkumar.k@vivriticapital.com>
2025-08-21 07:58:59 +0530

    Commit from rebase-branch

--- script2.py
+++ script2.py
@@ -1,3 +1,7 @@
 "print('Hello from script2')" 
 "print (feature branch update)" 
+<<<<<<< HEAD
 "print('Update from main branch')" 
+=======
+"print('Change from rebase-branch')" 
+>>>>>>> e873a86 (Commit from rebase-branch)
